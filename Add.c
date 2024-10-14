ddd95a914327d6bfb290b51086ae2aa34f5d5b59
Reyan-shaik77 <reyanshaik98@gmail.com>
2024-10-14 20:46:04 +0530

    Create Add.c

---
+++ Add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int n,m;
+int main()
+{
+  printf("enter first number");
+scanf("%d",&n);
+printf("enter second number");
+scanf("%d",&m);
+sum=n+m;
+printf("the sum of number is %d",sum);
